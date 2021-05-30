98bc5ef5d7d5aaf928f55cb2a6f1f5d75b4bf4f7
Heuler Felipe Ramos Manfredi <heuler.manfredi@vetta.digital>
2021-05-30 17:31:09 -0300

    Create Project

---
+++ src/main/java/com/orangetalents/vecontrol/VecontrolApplication.java
@@ -0,0 +1,13 @@
+package com.orangetalents.vecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VecontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangetalents/vecontrol/VecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangetalents.vecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
